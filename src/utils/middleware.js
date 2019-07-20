817a44f6d680468f4a2c76bd0abb07aed07279a4
Yoanm <yoanm@users.noreply.github.com>
2019-07-20 21:01:34 +0200

    Add disableRoleChecker listener option

--- src/utils/middleware.js
+++ src/utils/middleware.js
@@ -2,7 +2,7 @@
 
 const userHasRoles = (robot, response, user, requiredRoleList, listenerIdentifier) => {
     if (!user || !user.id) {
-        robot.logger.error('[Role checker] Unable to retrieve the user to check role !');
+        robot.logger.error(`[Role checker] Unable to retrieve the user to check role for ${listenerIdentifier}`);
 
         return false;
     }
@@ -22,28 +22,37 @@ const userHasRoles = (robot, response, user, requiredRoleList, listenerIdentifie
 
 module.exports = (robot, context, next, done) => {
     const {listener = {}, response = {}} = context;
-    const {options: {role: requiredRole = null}, regex: listenerRegex = undefined} = listener;
+    const {options: {role: requiredRole = null, disableRoleChecker = false}, regex: listenerRegex = undefined} = listener;
     const {message: {user = {}} = {}} = response;
 
-    try {
-        const requiredRoleList = [];
-        if (robot.roleChecker.getDefaultRequiredRole()) {
-            requiredRoleList.push(robot.roleChecker.getDefaultRequiredRole());
-        }
-        if (!!(requiredRole)) {
-            requiredRoleList.push(requiredRole);
-        }
-        if (requiredRoleList.length) {
-            const listenerIdentifier = (listener.options && listener.options.id)
-                ? listener.options.id
-                : JSON.stringify({options: listener.options, regex: (listenerRegex && listenerRegex.toString())})
+    // Will be used in two places but better to do it ONLY if useful
+    const computeListenerIdentifier = () => {
+        return (listener.options && listener.options.id)
+            ? listener.options.id
+            : JSON.stringify({options: listener.options, regex: (listenerRegex && listenerRegex.toString())})
             ;
-            robot.logger.debug(`[Role checker] start checking listener ${listenerIdentifier} with roles: ${JSON.stringify(requiredRoleList)}`);
-            if (!userHasRoles(robot, response, user, requiredRoleList, listenerIdentifier)) {
-                done();
-                return;
+    };
+
+    try {
+        if (disableRoleChecker !== true) {
+            const requiredRoleList = [];
+            if (robot.roleChecker.getDefaultRequiredRole()) {
+                requiredRoleList.push(robot.roleChecker.getDefaultRequiredRole());
             }
-            robot.logger.debug('[Role checker] Check successful');
+            if (!!(requiredRole)) {
+                requiredRoleList.push(requiredRole);
+            }
+            if (requiredRoleList.length) {
+                const listenerIdentifier = computeListenerIdentifier();
+                robot.logger.debug(`[Role checker] start checking listener "${listenerIdentifier}" with roles: ${JSON.stringify(requiredRoleList)}`);
+                if (!userHasRoles(robot, response, user, requiredRoleList, listenerIdentifier)) {
+                    done();
+                    return;
+                }
+                robot.logger.debug('[Role checker] Check successful');
+            }
+        } else {
+            robot.logger.debug(`[Role checker] disabled by option for listener "${computeListenerIdentifier()}"`);
         }
         next();
     } catch (error) {
